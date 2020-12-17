3798451e59cb471ac56f1ff5e74171eb64ab8ad7
一条小团团 <60549575+mymoonyue@users.noreply.github.com>
2020-12-17 14:22:31 +0800

    Update Subconverter.vue

--- src/views/Subconverter.vue
+++ src/views/Subconverter.vue
@@ -268,17 +268,15 @@ export default {
         },
         customBackend: {
           "localhost:25500 本地版": "http://localhost:25500/sub?",
-	  "api.cscong.cn (一条小团团自用）": "https://api.cscong.cn/sub?",
-          "sub-beta.now.sh (自动编译最新版本后端-测试）": "https://sub-beta.now.sh/sub?",
-          "subcon.dlj.tf(subconverter作者提供-稳定)":
+          "api.cscong.cn (一条小团团自用-测试）": "https://api.cscong.cn/sub?",
+          "subcon.dlj.tf(subconverter作者提供)":
             "https://subcon.dlj.tf/sub?",
-          "api.dler.io(sub作者&lhie1提供-稳定)": "https://api.dler.io/sub?",
-          "api.wcc.best(sub-web作者提供-稳定)": "https://api.wcc.best/sub?",
+          "api.dler.io(sub作者&lhie1提供)": "https://api.dler.io/sub?",
+          "api.wcc.best(sub-web作者提供)": "https://api.wcc.best/sub?",
         },
         backendOptions: [
           { value: "http://localhost:25500/sub?" },
-	  { value: "https://api.cscong.cn/sub?" },
-          { value: "https://sub-beta.now.sh/sub?" },
+          { value: "https://api.cscong.cn/sub?" },
           { value: "https://subcon.dlj.tf/sub?" },
           { value: "https://api.dler.io/sub?" },
           { value: "https://api.wcc.best/sub?" },
